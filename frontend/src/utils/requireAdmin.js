5b910d6ffeb17dc58db2aff8cda578c283a4698a
Sushi2209 <kshivani92000@gmail.com>
2026-01-30 11:56:38 +0530

    requireAdmin.js

--- frontend/src/utils/requireAdmin.js
+++ frontend/src/utils/requireAdmin.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 export default function requireAdmin() {
   const token = localStorage.getItem("admin_token");
   if (!token) {
     window.location.href = "/admin/login";
   }
 }
-=======
-export default function requireAdmin() {
-  const token = localStorage.getItem("admin_token");
-  if (!token) {
-    window.location.href = "/admin/login";
-  }
-}
->>>>>>> 1fcef1a213d1ccc9cdb3d83d609f27b065438bdd
