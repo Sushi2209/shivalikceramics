17a3eb42a01381a955280d12ebc8b048422a046b
Sushi2209 <kshivani92000@gmail.com>
2026-01-30 11:31:53 +0530

    whatsapp_service.py

--- backend/whatsapp_service.py
+++ backend/whatsapp_service.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import ssl
 import certifi
 import os
@@ -36,42 +34,3 @@ def send_whatsapp_alert(data: dict):
         from_=FROM,
         to=TO
     )
-=======
-
-import ssl
-import certifi
-import os
-os.environ["SSL_CERT_FILE"] = certifi.where()
-from twilio.rest import Client
-
-
-# Fix SSL certificate verification (Windows only)
-ssl_context = ssl.create_default_context(cafile=certifi.where())
-
-client = Client(
-    os.environ["TWILIO_SID"],
-    os.environ["TWILIO_AUTH_TOKEN"],
-    http_client=None,  # Twilio uses default HTTPS handler
-)
-
-FROM = os.environ["TWILIO_WHATSAPP_FROM"]
-TO = os.environ["ADMIN_WHATSAPP"]
-
-def send_whatsapp_alert(data: dict):
-    message = f"""
-📩 *New Contact Inquiry*
-
-👤 Name: {data['name']}
-📞 Phone: {data['phone']}
-📧 Email: {data.get('email', 'N/A')}
-
-💬 Message:
-{data['message']}
-    """
-
-    client.messages.create(
-        body=message,
-        from_=FROM,
-        to=TO
-    )
->>>>>>> 1fcef1a213d1ccc9cdb3d83d609f27b065438bdd
