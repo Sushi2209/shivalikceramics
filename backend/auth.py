7b285625b0d98c5c384c69e362efdd0a0459606f
Sushi2209 <kshivani92000@gmail.com>
2026-01-30 11:14:21 +0530

    auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from datetime import datetime, timedelta
 from jose import jwt
 import os
@@ -21,27 +21,3 @@ def create_token():
 
 def verify_token(token: str):
     return jwt.decode(token, SECRET, algorithms=[ALGO])
-=======
-from datetime import datetime, timedelta
-from jose import jwt
-import os
-from dotenv import load_dotenv
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).parent
-load_dotenv(ROOT_DIR / ".env")
-
-
-SECRET = os.environ["JWT_SECRET"]
-ALGO = "HS256"
-
-def create_token():
-    payload = {
-        "role": "admin",
-        "exp": datetime.utcnow() + timedelta(hours=8)
-    }
-    return jwt.encode(payload, SECRET, algorithm=ALGO)
-
-def verify_token(token: str):
-    return jwt.decode(token, SECRET, algorithms=[ALGO])
->>>>>>> 1fcef1a213d1ccc9cdb3d83d609f27b065438bdd
