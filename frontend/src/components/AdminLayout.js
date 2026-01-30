934a9e7e7cd3612532cbab0715a44127937db4bf
Sushi2209 <kshivani92000@gmail.com>
2026-01-30 11:51:59 +0530

    AdminLayout.js

--- frontend/src/components/AdminLayout.js
+++ frontend/src/components/AdminLayout.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 export default function AdminLayout({ children }) {
@@ -15,21 +15,3 @@ export default function AdminLayout({ children }) {
     </div>
   );
 }
-=======
-import React from "react";
-
-export default function AdminLayout({ children }) {
-  return (
-    <div className="min-h-screen bg-slate-100">
-      
-      {/* Spacer to avoid overlap with main navbar */}
-      <div className="h-24" />
-
-      {/* Main Content */}
-      <main className="mt-10 p-8 max-w-7xl mx-auto bg-white rounded-lg shadow-sm">
-        {children}
-      </main>
-    </div>
-  );
-}
->>>>>>> 1fcef1a213d1ccc9cdb3d83d609f27b065438bdd
